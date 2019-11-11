7e95010145560ba3f1059ca8480282524d0e1fa2
Andrey Klokov <anklog.98@yandex.ru>
2019-11-11 17:30:54 +0300

    Changed package names

--- src/com/file1/dimensial/array/one/ArrayTest.java
+++ src/com/file1/dimensial/array/one/ArrayTest.java
@@ -1,7 +1,7 @@
 package com.file1.dimensial.array.one;
 
 public class ArrayTest {
-    public ArrayTest(){
+    public ArrayTest(){         //test class
         Array array = new Array();
         array.Odd();
         array.numberOfOddAndEven();
